81e37fac3c990475df43b480058798cf78d303c4
victorkongsbak <victoreskongsbak@gmail.com>
2021-11-08 10:15:49 +0100

    basic branches added

--- app/src/main/java/com/example/bkskjold/ui/view/pages/TrainingOverviewPage.kt
+++ app/src/main/java/com/example/bkskjold/ui/view/pages/TrainingOverviewPage.kt
@@ -1,4 +1,10 @@
 package com.example.bkskjold.ui.view.pages
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bkskjold.ui.theme.BKSkjoldTheme
+import com.example.bkskjold.ui.view.reusables.TrainingCard
+
 class TrainingOverviewPage {
+
 }
