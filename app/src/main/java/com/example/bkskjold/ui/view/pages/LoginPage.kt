ab0018c8f14c1a82f80ad954a80c0741820693fd
Pernille <s205361@student.dtu.dk>
2021-11-25 15:21:38 +0100

    Added password textfield, "log ind" button and register button

--- app/src/main/java/com/example/bkskjold/ui/view/pages/LoginPage.kt
+++ app/src/main/java/com/example/bkskjold/ui/view/pages/LoginPage.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
@@ -11,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -24,12 +26,18 @@ fun LoginPage() {
     val image = painterResource(id = R.drawable.login_image)
 
     var emailValue by remember {mutableStateOf("")}
+    var passwordValue by remember { mutableStateOf("")}
 
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter) {
+
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color.White), contentAlignment = Alignment.TopCenter
+                .background(Color.White),
+            contentAlignment = Alignment.TopCenter
         ) {
 
             Image(image, contentDescription = "login_image")
@@ -50,10 +58,51 @@ fun LoginPage() {
             )
 
             OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .fillMaxHeight(0.13f),
                 value = emailValue,
                 onValueChange = {emailValue = it},
-                label = {Text("Email adresse")},
+                label = null,
+                placeholder = { Text(text = "Email adresse")},
+                shape = RoundedCornerShape(50)
+
             )
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .fillMaxHeight(0.13f),
+                value = passwordValue,
+                onValueChange = {passwordValue = it},
+                label = null,
+                placeholder = { Text(text = "Adgangskode")},
+                shape = RoundedCornerShape(50)
+            )
+
+            Button(onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .fillMaxHeight(0.13f),
+                shape = RoundedCornerShape(50)
+
+            ) {
+               Text(text = "Log ind")
+
+            }
+            Text(
+                text = "------------------------- Eller -------------------------",
+            )
+            Button(onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .fillMaxHeight(0.13f),
+                shape = RoundedCornerShape(50)
+
+            ) {
+                Text(text = "Registrér dig")
+
+            }
+
         }
     }
 }
