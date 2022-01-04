3516c3ed8dc83786fb07a1adadb51fec456c8cd3
Kongsbak99 <victoreskongsbak@gmail.com>
2022-01-04 19:19:09 +0100

    test commit - event page done

--- app/src/main/java/com/example/bkskjold/ui/view/pages/TrainingOverviewPage.kt
+++ app/src/main/java/com/example/bkskjold/ui/view/pages/TrainingOverviewPage.kt
@@ -1,6 +1,7 @@
 package com.example.bkskjold.ui.view.pages
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.ui.unit.*
 
 import androidx.compose.material.Button
