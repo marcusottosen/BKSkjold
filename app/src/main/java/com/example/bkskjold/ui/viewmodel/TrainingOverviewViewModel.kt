6f3811e865c6fb6f0a048a66074513cd1c1f4784
Kongsbak99 <victoreskongsbak@gmail.com>
2022-01-05 14:51:20 +0100

    Initial logic for filter and sort function for trainingoverview page. No visual implementation yet.

--- app/src/main/java/com/example/bkskjold/ui/viewmodel/TrainingOverviewViewModel.kt
+++ app/src/main/java/com/example/bkskjold/ui/viewmodel/TrainingOverviewViewModel.kt
@@ -10,6 +10,8 @@ import androidx.navigation.NavController
 import com.example.bkskjold.R
 import com.example.bkskjold.data.model.getSignedUpTrainings
 import com.example.bkskjold.data.model.trainings
+import com.example.bkskjold.data.model.BookingData
+import com.example.bkskjold.data.model.Training
 import com.example.bkskjold.ui.view.reusables.TrainingCard
 
 class TrainingOverviewViewModel {
@@ -31,6 +33,41 @@ class TrainingOverviewViewModel {
             }
             item {Spacer(modifier = Modifier.height(80.dp))}
         }
+    }
+
+    @Composable
+    fun filterAndSort(navController: NavController, date: Int? = null, timeStart: Int? = null, overviewOrSignedUp: Boolean = false){
+
+
+        //First decide which list of practices to filter through
+        var practices = if (overviewOrSignedUp == true){
+            BookingData().getSignedUpTrainings()
+        } else{
+            BookingData().bookings
+        }
+
+        var filteredPractices: MutableList<Training> = mutableListOf()
+        var date = date.toString()
+        var timestamp = timeStart.toString()
+
+
+        try {
+            for (i in practices){
+                if(i.date == date!! && i.timeStart == timestamp!!){
+                    filteredPractices.add(i)
+                }
+            }
+        } catch (e: Exception){
+
+        }
+
+        //Finally create the view with the right practices.
+        LazyColumn {
+            items(practices.size) { i ->
+                TrainingCard(training = practices[i], navController)
+            }
+        }
+
     }
 }
 
