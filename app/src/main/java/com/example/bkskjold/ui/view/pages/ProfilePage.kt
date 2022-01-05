a6ece1ff4656da00185db614821c0f880d4c52bc
UnknownBrain <gufa1353@gmail.com>
2022-01-05 10:01:09 +0100

    Profile page ready to push

--- app/src/main/java/com/example/bkskjold/ui/view/pages/ProfilePage.kt
+++ app/src/main/java/com/example/bkskjold/ui/view/pages/ProfilePage.kt
@@ -72,10 +72,15 @@ fun ProfileOverview() {
         item {
             NextTrainingCard(
                 header = "Træning for seniorer",
-                description = "Holdtræning",
-                date = "25. Oktober 2021",
-                time = "17:45",
-                location = "Bane C"
+                date = "25. oktober",
+                timeStart = "16:00",
+                timeEnd = "17:30",
+                day = "mandag",
+                team = "U12",
+                location = "Bane C",
+                attending = 8,
+                spots = 12,
+                trainer = "Bjarne Andersen"
             )
         }
 
