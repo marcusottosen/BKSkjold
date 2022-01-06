771189317075a2306a61c39e0eaf7c8e109055f7
Pernille <s205361@student.dtu.dk>
2022-01-06 16:52:44 +0100

    LoginFrontPage created

---
+++ app/src/main/java/com/example/bkskjold/ui/view/pages/LoginFrontPage.kt
@@ -0,0 +1,20 @@
+package com.example.bkskjold.ui.view.pages
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.bkskjold.R
+
+@Composable
+fun LoginFrontPage() {
+
+}
