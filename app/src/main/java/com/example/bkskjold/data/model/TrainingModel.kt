c20cff4861480c79972be2e4fc85afdd92da6741
Marcus <mkottosen@gmail.com>
2022-01-06 16:09:58 +0100

    TrainingCards are now created from data straight from the Database! Filter on attending training works too.

--- app/src/main/java/com/example/bkskjold/data/model/TrainingModel.kt
+++ app/src/main/java/com/example/bkskjold/data/model/TrainingModel.kt
@@ -1,58 +1,12 @@
 package com.example.bkskjold.data.model
-import android.content.ContentValues.TAG
+import android.content.ContentValues
 import android.os.Parcelable
 import android.util.Log
-import androidx.compose.runtime.snapshots.SnapshotStateList
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestoreException
-import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.parcelize.Parcelize
 
-class BookingData {
-    /*
-    var training1  = listOf(1 , "21:00", "22:00", "mandag", "25 oktober", "Bane C", "Senior", "Træner: Ekkart", 0, 20, "Normal træning for u13. Kom i god tid!", 12, 2, 3)
-    var training2  = listOf(2 , "17:00", "22:00", "mandag", "25 oktober", "Bane A", "U20", "Træner: Ekkart", 1, 20, "Normal træning for u13. Kom i god tid!", 12, 2, 3)
-    var training3  = listOf(3 , "17:00", "22:00", "mandag", "25 oktober", "Bane B", "U20", "Træner: Ekkart", 0, 20, "Normal træning for u13. Kom i god tid!", 12, 2, 3)
-    var training4  = listOf(4 , "17:00", "22:00", "mandag", "25 oktober", "Bane C", "U20", "Træner: Ekkart", 1, 20, "Normal træning for u13. Kom i god tid!", 12, 2, 3)
-    var training5  = listOf(5 , "18:00", "22:00", "mandag", "25 oktober", "Bane C", "U20", "Træner: Ekkart", 1, 20, "Normal træning for u13. Kom i god tid!", 12, 2, 3)
-    var training6  = listOf(6 , "17:00", "22:00", "mandag", "25 oktober", "Bane D", "U20", "Træner: Ekkart", 1, 20, "Normal træning for u13. Kom i god tid!", 12, 2, 3)
-    var training7  = listOf(7 , "17:00", "22:00", "mandag", "25 oktober", "Bane C", "U20", "Træner: Ekkart", 0, 20, "Normal træning for u13. Kom i god tid!", 12, 2, 3)
-    var training8  = listOf(8 , "17:00", "22:00", "mandag", "25 oktober", "Bane F", "U20", "Træner: Ekkart", 0, 20, "Normal træning for u13. Kom i god tid!", 12, 2, 3)
-    var training9  = listOf(9 , "17:00", "22:00", "mandag", "25 oktober", "Bane C", "U20", "Træner: Ekkart", 0, 20, "Normal træning for u13. Kom i god tid!", 12, 2, 3)
-    var training10 = listOf(10, "16:30", "22:00", "mandag", "26 oktober", "Bane A", "U20", "Træner: Ekkart", 1, 20, "Normal træning for u13. Kom i god tid!", 12, 2, 3)
-    var training11 = listOf(11, "17:00", "22:00", "mandag", "26 oktober", "Bane C", "U20", "Træner: Ekkart", 0, 20, "Normal træning for u13. Kom i god tid!", 12, 2, 3)
-    var training12 = listOf(12, "17:00", "22:00", "mandag", "26 oktober", "Bane D", "U20", "Træner: Ekkart", 0, 20, "Normal træning for u13. Kom i god tid!", 12, 2, 3)
-
-    fun getAllTraining(): List<List<Any>> {
-        /* TODO This should eventually fetch the data from a database.
-            This should be all available training, including the ones
-             already signed up for. This should take a parameter,
-             to find the specific users data and trainings.*/
-        var allTrainings = listOf(training1, training2, training3, training4, training5, training6, training7, training8, training9, training10, training11, training12)
-
-        return allTrainings
-    }
-
-    fun getSignedUpTrainings(): MutableList<List<Any>>? { //
-    /* TODO This should eventually fetch the data from a database.
-            Data should include all the current trainings a user has signed up for.
-            This method should probably take a parameter, to find the correct user and data */
-        var allTrainings = getAllTraining()
-        var signedUpTrainings: MutableList<List<Any>> = mutableListOf()
-
-        for (i in allTrainings.indices){
-            var current = allTrainings[i]
-
-            if(current[7] == 0){
-                signedUpTrainings?.add(current)
-            }
-        }
-        return signedUpTrainings
-    }*/
-
-    //TODO Fetch from database instead
+//class BookingData {
     /*val bookings = listOf(
         Training(1 , "21:00", "22:00", "mandag", "25 oktober", "Bane C", "Senior", "Træner: Ekkart", false, 20, "Normal træning for u13. Kom i god tid!", 12, 2, 3),
         Training(2 , "17:00", "22:00", "mandag", "25 oktober", "Bane A", "U20", "Træner: Ekkart", true, 20, "Normal træning for u13. Kom i god tid!", 12, 2, 3),
@@ -67,75 +21,90 @@ class BookingData {
         Training(11, "17:00", "22:00", "mandag", "26 oktober", "Bane C", "U20", "Træner: Ekkart", false, 20, "Normal træning for u13. Kom i god tid!", 12, 2, 3),
         Training(12, "17:00", "22:00", "mandag", "26 oktober", "Bane D", "U20", "Træner: Ekkart", false, 20, "Normal træning for u13. Kom i god tid!", 12, 2, 3)
     )*/
-    val bookings = mutableListOf<Training>()
-
-    /*fun loadTrainingsFromDB(){
-        val db = Firebase.firestore
+    /*
+fun writeToDB() {
+    val db = Firebase.firestore
+    for (i in 1..trainings.size) {
         db.collection("trainings")
-            .get()
-            .addOnSuccessListener { result ->
-                for (training in result){
-                    bookings.updateList(result.toObjects(Training::class.java))
-                }
+            .add(trainings[i-1])
+            .addOnSuccessListener { documentReference ->
+                Log.d(
+                    ContentValues.TAG,
+                    "DocumentSnapshot added with ID: ${documentReference.id}"
+                )
             }
-
-    }*/
-
-    fun loadTrainingsFromDB() {
-        FirebaseFirestore
-            .getInstance()
-            .collection("trainings")
-            .addSnapshotListener(this,
-                { querySnapshot: QuerySnapshot?, e: FirebaseFirestoreException? ->
-                    for (instance in querySnapshot.documents){
-                        val Training = document.toObject(Training::class.java)
-                    }
-
-
-                })
-
-
-
-        fun getAllTrainings(){
-
+            .addOnFailureListener { e ->
+                Log.w(ContentValues.TAG, "Error adding document", e)
+            }
     }
+}*/
+//}
 
-    fun getSignedUpTrainings(): MutableList<Training> {
-        loadTrainingsFromDB()
-        var bookings = bookings
-        var signedUpTrainings: MutableList<Training> = mutableListOf()
+val trainings: MutableList<Training> = mutableListOf()
 
-        for (training in bookings){
-            if (training.isAttending){
-                signedUpTrainings.add(training)
+fun loadTrainingsFromDB(): MutableList<Training>{
+
+    val db = Firebase.firestore
+    db.collection("trainings")
+        .get()
+        .addOnSuccessListener { result ->
+            for (doc in result) {
+                val test : Number = doc["price"] as Number
+                //val training = doc.toObject(Training::class.java) //Virker hvis der ikke bruges Int
+                //trainings.add(training)
+
+                trainings.add(
+                    Training(
+                        timeStart       = doc["timeStart"] as String,
+                        timeEnd         = doc["timeEnd"] as String,
+                        weekday         = doc["weekday"] as String,
+                        date            = doc["date"] as String,
+                        location        = doc["location"] as String,
+                        league          = doc["league"] as String,
+                        trainer         = doc["trainer"] as String,
+                        attending       = doc["attending"] as Boolean,
+                        price           = (doc["price"] as Number).toInt(),
+                        description     = doc["description"] as String,
+                        maxParticipants = (doc["maxParticipants"] as Number).toInt(),
+                        team1           = (doc["team1"] as Number).toInt(),
+                        team2           = (doc["team2"] as Number).toInt(),
+                        id              = (doc["id"] as Number).toInt()
+                    )
+                )
+                println(trainings.size)
             }
         }
-        return signedUpTrainings
-
+        .addOnFailureListener { exception ->
+            Log.d(ContentValues.TAG, "Error getting documents: ", exception)
+        }
+    return trainings
 }
 
+fun getSignedUpTrainings(): MutableList<Training> {
+    val signedUpTrainings: MutableList<Training> = mutableListOf()
 
-
+    for (training in trainings){
+        if (training.attending){
+            signedUpTrainings.add(training)
+        }
+    }
+    return signedUpTrainings
+}
 
 @Parcelize
 data class Training(
-    val id: Int,
-    val timeStart: String,
-    val timeEnd: String,
-    val weekday: String,
-    val date: String,
-    val location: String,
-    val league: String,
-    val trainer: String,
-    val isAttending: Boolean,
+    val id: Int = 0,
+    val timeStart: String = "",
+    val timeEnd: String= "",
+    val weekday: String= "",
+    val date: String= "",
+    val location: String= "",
+    val league: String= "",
+    val trainer: String= "",
+    val attending: Boolean = false,
     val price: Int,
-    val description: String,
-    val maxParticipants: Int,
-    val team1: Int,
-    val team2: Int
-    ): Parcelable{
-        fun <T> SnapshotStateList<T>.updateList(newData: List<T>){
-            clear()
-            addAll(newData)
-        }
-    }
+    val description: String= "",
+    val maxParticipants: Int = 0,
+    val team1: Int = 0,
+    val team2: Int = 0
+): Parcelable
--- app/src/main/java/com/example/bkskjold/ui/viewmodel/TrainingOverviewViewModel.kt
+++ app/src/main/java/com/example/bkskjold/ui/viewmodel/TrainingOverviewViewModel.kt
@@ -1,7 +1,5 @@
 package com.example.bkskjold.ui.viewmodel
 
-import android.content.ContentValues
-import android.util.Log
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,129 +8,29 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.bkskjold.R
-import com.example.bkskjold.data.model.BookingData
+import com.example.bkskjold.data.model.getSignedUpTrainings
+import com.example.bkskjold.data.model.trainings
 import com.example.bkskjold.ui.view.reusables.TrainingCard
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 
 class TrainingOverviewViewModel {
-    fun getColor(cards: List<List<String>>, i: Int): Int { //Used to fetch the correct color corresponding to participating status
-        if (cards[i][7] == "Deltag") {
-            return R.color.notParticipating
-        } else {
-            return R.color.participating
-        }
-
-    }
-
-
-    /* TODO These functions should eventually be altered
-        The idea is that instead of hardcoding the data,
-        it should be fetched from the model
-    */
     @Composable
     fun GetOverviewView(navController: NavController){
-        val trainings = BookingData().bookings
-
-        fun writeToDB() {
-            val db = Firebase.firestore
-
-
-            for (i in 1..trainings.size) {
-                db.collection("trainings")
-                    .add(trainings[i-1])
-                    .addOnSuccessListener { documentReference ->
-                        Log.d(
-                            ContentValues.TAG,
-                            "DocumentSnapshot added with ID: ${documentReference.id}"
-                        )
-                    }
-                    .addOnFailureListener { e ->
-                        Log.w(ContentValues.TAG, "Error adding document", e)
-                    }
-            }
-        }
-
-
         LazyColumn {
             items(trainings.size) { i ->
                 TrainingCard(training = trainings[i], navController)
             }
             item { Spacer(modifier = Modifier.height(80.dp)) }
         }
-/*
-        val allTrainings = BookingData().getAllTraining()
-        LazyColumn {
-            items(allTrainings.size) { i ->
-                TrainingCard( //create the cards
-                    id          = allTrainings[i][0],
-                    timeStart   = allTrainings[i][1],
-                    timeEnd     = allTrainings[i][2],
-                    weekday     = allTrainings[i][3],
-                    date        = allTrainings[i][4],
-                    participants= allTrainings[i][5],
-                    location    = allTrainings[i][6],
-                    league      = allTrainings[i][7],
-                    trainer     = allTrainings[i][8],
-                    attending      = allTrainings[i][9],
-                    color       = colorResource(id = getColor(allTrainings, i))
-                )
-            }
-        }*/
-        writeToDB()
     }
     @Composable
     fun GetSignedUpView(navController: NavController){
-        val trainings = BookingData().getSignedUpTrainings()
+        val trainings = getSignedUpTrainings()
         LazyColumn {
             items(trainings.size) { i ->
                 TrainingCard(training = trainings[i], navController)
             }
             item {Spacer(modifier = Modifier.height(80.dp))}
         }
-
-/*        val signedUpTrainings = BookingData().getSignedUpTrainings()
-        LazyColumn {
-            if (signedUpTrainings != null) {
-                items(signedUpTrainings.size) { i ->
-                    //TrainingCard(time, daysTillTraining, date, participants, location, league, trainer, button, colorResource(color))
-                    TrainingCard( //create the cards
-                        id          = signedUpTrainings[i][0],
-                        timeStart   = signedUpTrainings[i][1],
-                        timeEnd     = signedUpTrainings[i][2],
-                        weekday     = signedUpTrainings[i][3],
-                        date        = signedUpTrainings[i][4],
-                        participants= signedUpTrainings[i][5],
-                        location    = signedUpTrainings[i][6],
-                        league      = signedUpTrainings[i][7],
-                        trainer     = signedUpTrainings[i][8],
-                        attending      = signedUpTrainings[i][9],
-                        color = colorResource(id = getColor(signedUpTrainings, i))
-                    )
-                }
-            }else{
-                print("$signedUpTrainings was null")
-            /* TODO handle null exception */
-            }
-        }*/
     }
-
-    val user = hashMapOf(
-        "first" to "Ada",
-        "last" to "Lovelace",
-        "born" to 1815
-    )
-
-    /*fun writeToDB() {
-        val db = Firebase.firestore
-
-        db.collection("trainings")
-            .add(user)
-            .addOnSuccessListener { documentReference ->
-                Log.d(ContentValues.TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-            }
-            .addOnFailureListener { e ->
-                Log.w(ContentValues.TAG, "Error adding document", e)
-            }
-    }*/
 }
+
