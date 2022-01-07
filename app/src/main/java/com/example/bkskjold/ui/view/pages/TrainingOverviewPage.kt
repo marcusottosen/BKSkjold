7a4312f5ea3e9157098067dc5f7c5ec00d40fa9c
Kongsbak99 <victoreskongsbak@gmail.com>
2022-01-07 16:17:04 +0100

    import fixes

--- app/src/main/java/com/example/bkskjold/ui/view/pages/TrainingOverviewPage.kt
+++ app/src/main/java/com/example/bkskjold/ui/view/pages/TrainingOverviewPage.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
