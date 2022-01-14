7c6a88889a80bbd50db79f782f51d8793a6f372e
Pernille <s205361@student.dtu.dk>
2022-01-14 15:18:28 +0100

    Row with back icon added

--- app/src/main/java/com/example/bkskjold/ui/view/pages/MapPage.kt
+++ app/src/main/java/com/example/bkskjold/ui/view/pages/MapPage.kt
@@ -1,11 +1,30 @@
 package com.example.bkskjold.ui.view.pages
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.bkskjold.R
 
 @Composable
 fun MapPage() {
 
     val map = painterResource(id = R.drawable.banekort)
+
+    Row(modifier = Modifier.padding(10.dp), horizontalArrangement = Arrangement.Start) {
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                imageVector = Icons.Filled.ArrowBack,
+                contentDescription = null
+            )
+
+        }
+    }
 }
