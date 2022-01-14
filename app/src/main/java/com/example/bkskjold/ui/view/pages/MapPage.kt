b75011c77e839211487dff54836f17a865ece4e7
Pernille <s205361@student.dtu.dk>
2022-01-14 15:03:06 +0100

    Resource image "banekort" added

--- app/src/main/java/com/example/bkskjold/ui/view/pages/MapPage.kt
+++ app/src/main/java/com/example/bkskjold/ui/view/pages/MapPage.kt
@@ -1,9 +1,11 @@
 package com.example.bkskjold.ui.view.pages
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import com.example.bkskjold.R
 
 @Composable
 fun MapPage() {
 
-
+    val map = painterResource(id = R.drawable.banekort)
 }
