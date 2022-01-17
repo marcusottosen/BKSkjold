ac9a2e7312cdeda57ee669554c1ffc4c43599459
Pernille <s205361@student.dtu.dk>
2022-01-17 08:23:52 +0100

    Column with Text added

--- app/src/main/java/com/example/bkskjold/ui/view/pages/MapPage.kt
+++ app/src/main/java/com/example/bkskjold/ui/view/pages/MapPage.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
@@ -13,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bkskjold.R
 
 @Composable
@@ -37,6 +39,18 @@ fun MapPage() {
     )
     {
         item {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .padding(24.dp)
+            ){
+                Text(
+                text = "FS Banekort: Efterår 2021 - tfr",
+                color = Color.Black,
+                fontSize = 18.sp
+            )
+
+            }
 
         }
     }
