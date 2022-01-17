94ca283ace3b26353fc30d755236331b03e7546e
Kongsbak99 <victoreskongsbak@gmail.com>
2022-01-17 10:07:49 +0100

    unittest init

---
+++ app/src/test/java/com/example/bkskjold/ui/viewmodel/TrainingOverviewViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.example.bkskjold.ui.viewmodel
+
+import junit.framework.TestCase
+
+class TrainingOverviewViewModelTest : TestCase() {
+
+    fun testGetOverviewView() {}
+
+    fun testGetSignedUpView() {}
+
+    fun testFilterAndSort() {}
+}
