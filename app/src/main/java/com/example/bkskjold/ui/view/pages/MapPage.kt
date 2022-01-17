e3d087228e656abd2699286d7a1070178e2bead8
Pernille <s205361@student.dtu.dk>
2022-01-17 08:26:38 +0100

    Box with image added

--- app/src/main/java/com/example/bkskjold/ui/view/pages/MapPage.kt
+++ app/src/main/java/com/example/bkskjold/ui/view/pages/MapPage.kt
@@ -1,6 +1,8 @@
 package com.example.bkskjold.ui.view.pages
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Icon
@@ -11,7 +13,10 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,15 +48,34 @@ fun MapPage() {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .padding(24.dp)
-            ){
+            ) {
                 Text(
-                text = "FS Banekort: Efterår 2021 - tfr",
-                color = Color.Black,
-                fontSize = 18.sp
-            )
+                    text = "FS Banekort: Efterår 2021 - tfr",
+                    color = Color.Black,
+                    fontSize = 18.sp
+                )
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .padding(24.dp)
+                        .border(2.dp, Color.Black, RectangleShape)
+                        .fillMaxWidth()
+                        .background(colorResource(id = R.color.primary))
+
+                ) {
+
+
+                    Image(
+                        painter = map,
+                        contentDescription = "Map of football fields",
+                        modifier = Modifier
+                            .clip(RectangleShape)
+                    )
+
+                }
+
 
             }
-
         }
     }
 }
