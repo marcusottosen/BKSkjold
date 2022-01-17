fc9e37086df9fde1951e07f19ca8d6ae37124d36
Pernille <s205361@student.dtu.dk>
2022-01-17 08:21:40 +0100

    LazyColumn added

--- app/src/main/java/com/example/bkskjold/ui/view/pages/MapPage.kt
+++ app/src/main/java/com/example/bkskjold/ui/view/pages/MapPage.kt
@@ -1,14 +1,16 @@
 package com.example.bkskjold.ui.view.pages
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.bkskjold.R
@@ -24,6 +26,17 @@ fun MapPage() {
                 imageVector = Icons.Filled.ArrowBack,
                 contentDescription = null
             )
+
+        }
+    }
+    LazyColumn(
+        horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(1.0f)
+            .background(Color.Transparent)
+    )
+    {
+        item {
 
         }
     }
